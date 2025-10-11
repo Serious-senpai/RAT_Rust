d9f34471482643db3f3acf52463fbf46841cd307
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2025-10-11 03:48:12 +0000

    Add RAT build script

---
+++ rat/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#! https://stackoverflow.com/a/246128
+RAT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+echo "Got rat directory: $RAT_DIR"
+
+cargo build --release --target x86_64-unknown-linux-gnu
+mv target/x86_64-unknown-linux-gnu/release/agent target/agent.linux_x86_64
+
+rm -rf bundle.zip
+zip -j bundle.zip target/agent.linux_x86_64
